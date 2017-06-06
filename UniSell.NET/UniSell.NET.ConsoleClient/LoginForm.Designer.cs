e9a7f85f3f8967cbd783a94927770878a32bedb9
RaulEstrada <raulestradaballesteros@gmail.com>
2017-06-06 11:36:48 +0200

    Created Windows Form Application that consumes the Java User Management SOAP WS. This new client has only the Login page

---
+++ UniSell.NET/UniSell.NET.ConsoleClient/LoginForm.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace UniSell.NET.ConsoleClient
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.username_login = new System.Windows.Forms.TextBox();
+            this.password_login = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(287, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(61, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre de usuario:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(124, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contraseña:";
+            // 
+            // username_login
+            // 
+            this.username_login.Location = new System.Drawing.Point(260, 179);
+            this.username_login.Name = "username_login";
+            this.username_login.Size = new System.Drawing.Size(277, 20);
+            this.username_login.TabIndex = 3;
+            // 
+            // password_login
+            // 
+            this.password_login.Location = new System.Drawing.Point(260, 229);
+            this.password_login.Name = "password_login";
+            this.password_login.Size = new System.Drawing.Size(277, 20);
+            this.password_login.TabIndex = 4;
+            this.password_login.UseSystemPasswordChar = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::UniSell.NET.ConsoleClient.Properties.Resources.shopping_cart;
+            this.pictureBox1.Location = new System.Drawing.Point(158, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(96, 88);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.loginButton.Location = new System.Drawing.Point(422, 291);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(115, 34);
+            this.loginButton.TabIndex = 6;
+            this.loginButton.Text = "Acceder";
+            this.loginButton.UseVisualStyleBackColor = false;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 368);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.password_login);
+            this.Controls.Add(this.username_login);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LoginForm";
+            this.Text = "UniSell: Gestión de Usuarios";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox username_login;
+        private System.Windows.Forms.TextBox password_login;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button loginButton;
+    }
+}
+
---
+++ UniSell.NET/UniSell.NET.ConsoleClient/LoginForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UniSell.NET.ConsoleClient.UniSellWS;
+
+namespace UniSell.NET.ConsoleClient
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            string username = username_login.Text.ToString();
+            string password = password_login.Text.ToString();
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                showErrorMsg("Por favor, introduzca el nombre de usuario y contraseña", "Formulario no válido");
+            } else
+            {
+                UserWSClient ws = new UserWSClient();
+                string authToken = ws.login(username, password);
+                if (string.IsNullOrEmpty(authToken))
+                {
+                    showErrorMsg("Credenciales de acceso incorrectas", "Login Incorrecto");
+                }
+            }
+        }
+
+        private void showErrorMsg(string msg, string title)
+        {
+            MessageBox.Show(msg,
+                title,
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+        }
+    }
+}
